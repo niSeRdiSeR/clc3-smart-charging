773824d3e5b069740453bc3424e2787a05a3b9bd
SebastianGattinger <dbrnise@gmail.com>
2023-01-21 14:14:52 +0100

    removed unnecessary code in wp-handler

--- update-handler/wp-handler.py
+++ update-handler/wp-handler.py
@@ -3,10 +3,7 @@ import os
 import json
 import requests
 import redis
-from google.cloud import pubsub_v1
 
-PROJECT_ID = os.getenv('GCLOUD_PROJECT')
-TARGET_TOPIC = os.getenv('TARGET_TOPIC')
 REDIS_HOST = os.getenv('REDIS_HOST')
 
 def handle(event, context):
@@ -22,16 +19,6 @@ def handle(event, context):
 
     print(f"processing: {pubsub_message} @ {src_topic}")
 
-
-    project_path = f"projects/{PROJECT_ID}"
-    publisher = pubsub_v1.PublisherClient()
-
-    topic_path = publisher.topic_path(PROJECT_ID, TARGET_TOPIC)
-    topic_strings = [t.name for t in publisher.list_topics(request={"project": project_path})]
-
-    if topic_path not in topic_strings:
-        publisher.create_topic(request={"name": topic_path})
-    
     pk = message_json.pop('pk')
 
     r.hset(f"wp-{pk}", message_json['prop'], message_json['val'])
