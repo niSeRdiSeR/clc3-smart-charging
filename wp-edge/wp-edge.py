37bfb9e70c0fbe90d13de6957de61bd54ec60533
Sebastian G <s.gattinger@tmss.at>
2023-01-25 19:45:16 +0100

    dev

--- wp-edge/wp-edge.py
+++ wp-edge/wp-edge.py
@@ -1,4 +1,7 @@
+import os
 import sys
+import time
+import json
 import paho.mqtt.client as mqtt
 from datetime import datetime
 from google.cloud import pubsub_v1
@@ -22,14 +25,30 @@ sub_topic_name = f'projects/{PROJECT_ID}/topics/{SUB_TOPIC}'
 
 pub_topic_name = f'projects/{PROJECT_ID}/topics/{PUB_TOPIC}',
 
+
+last_vals = {}
+
+
 def mqtt_msg_handler(client, userdata, message):
+    print("MSG!")
     topic_list = message.topic.split('/')
     if 'properties' in topic_list[1]:
         prop = topic_list[3]
     elif 'available' in topic_list[1]:
         prop = 'available'
     val = str(message.payload.decode("utf-8"))
-    publisher.publish(pub_topic_name, json.dumps({"pk": WP_PK, "prop": prop, "val": val, "timestamp": datetime.utcnow().timestamp()}))
+    if last_vals.get(prop, None) is not val:
+        print(f"publishing to {pub_topic_name}")
+        data = json.dumps({"pk": WP_PK, "prop": str(prop), "val": val, "timestamp": datetime.utcnow().timestamp()})
+        print(data)
+        publisher.publish(pub_topic_name, data.encode('utf-8'))
+        last_vals[prop] = val
+
+try:
+    mqtt_client.connect(MQTT_IP)
+
+except:
+    print(f"Couldn't connect with mqtt @ {MQTT_IP}")
 
 mqtt_client.subscribe("wattpilot/properties/nrg_ptotal/state", qos=1) # float
 mqtt_client.subscribe("wattpilot/properties/psm/state", qos=1) # int
@@ -41,13 +60,9 @@ mqtt_client.subscribe("wattpilot/properties/wh/state", qos=1) # float
 mqtt_client.subscribe("wattpilot/available", qos=1) # online/offline
 
 mqtt_client.on_message=mqtt_msg_handler
+mqtt_client.loop_start()
 
-try:
-    mqtt_client.connect(MQTT_IP)
-    mqtt_client.loop_start()
-
-except:
-    print(f"Couldn't connect with mqtt @ {MQTT_IP}")
+print("loop up")
 
 def sub_msg_handler(msg):
     msg_json = json.loads(msg)
@@ -56,8 +71,17 @@ def sub_msg_handler(msg):
 
 try:
     subscription_name = f'projects/{PROJECT_ID}/subscriptions/wp-edge-sub'
-    subscriber.create_subscription(
-        name=subscription_name, topic=sub_topic_name)
+#    subscriber.create_subscription(
+#        name=subscription_name, topic=sub_topic_name)
     future = subscriber.subscribe(subscription_name, sub_msg_handler)
+#    try:
+#        print("blocking")
+#        future.result()
+#    except KeyboardInterrupt:
+#        print("canceled!")
+#        future.cancel()
 except:
     print(f"Couldn't subscribe to pub/sub with '{subscription_name}' @ {sub_topic_name}")
+
+while True:
+    time.sleep(60)
